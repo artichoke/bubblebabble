c20066c3ae81a5323bcdeb3ead507d7c478e4f8e
artichoke-ci <ci@artichokeruby.org>
2025-01-21 00:10:46 +0000

    deploy: 4bddcaaf6913cfde6e17c3975c2d4039f6c1419c

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["boba",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"boba/enum.DecodeError.html\" title=\"enum boba::DecodeError\">DecodeError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[260]}
