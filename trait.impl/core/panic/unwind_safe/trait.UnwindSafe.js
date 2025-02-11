24a3d287373769831400ba28878711fa3917129b
artichoke-ci <ci@artichokeruby.org>
2025-02-11 00:10:56 +0000

    deploy: 0f8cef647c9874b6aadaf257c268fa47e96a2640

---
+++ trait.impl/core/panic/unwind_safe/trait.UnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["boba",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"enum\" href=\"boba/enum.DecodeError.html\" title=\"enum boba::DecodeError\">DecodeError</a>",1,["boba::DecodeError"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[325]}
